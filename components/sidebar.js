c8e42d485f6d9887c49c3385f7ae305c58d43465
jacarp <91710233+jacarp@users.noreply.github.com>
2022-11-18 12:24:51 -0600

    started sidenav process

---
+++ components/sidebar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  CDBSidebar,
+  CDBSidebarContent,
+  CDBSidebarFooter,
+  CDBSidebarHeader,
+  CDBSidebarMenu,
+  CDBSidebarMenuItem,
+} from 'cdbreact';
+import { NavLink } from 'react-router-dom';
+
+const Sidebar = () => {
+  return (
+    <div></div>
+  );
+};
+
+export default Sidebar;
+
--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import './App.css';
+import Sidebar from './sidebar';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
-    return (
-        <div>
+  return (
+    <Router>
+      <div className="App">
 
-        </div>
-        
-    );
+      </div>
+    </Router>
+  );
 }
-  
+
 export default App;
+
