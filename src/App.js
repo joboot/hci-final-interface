3ee01bc2a5d119dfb28784127e0eb5149bcbf954
joboot <jordan.booth41900@gmail.com>
2022-11-18 12:46:31 -0600

    Sign in form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './index.css';
 import Sidebar from './components/sidebar';
+import SignIn from './pages/SignIn';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <div className="App">
-
+        <SignIn/>
       </div>
     </Router>
   );
