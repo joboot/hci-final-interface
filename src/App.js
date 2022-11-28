6bc272b8def3ddc414bfb12b3873440e928645be
brlewi03 <brlewi03@wsc.edu>
2022-11-28 12:29:04 -0600

    Updated Code that has a Header/Top Bar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import './index.css';
 import Sidebar from './components/sidebar';
 import SignIn from './pages/SignIn';
 import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import Header from "./components/TopBarComp";
+
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Header/>
         <SignIn/>
       </div>
     </Router>
--- src/components/TopBarComp.js
+++ src/components/TopBarComp.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react'
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
-export default class TopBarComp extends Component {
-    render() {
-      return (
-        <div></div>
-      )
-    }
-  }
+const Header=() => {
+  return (
+      <AppBar position="static">
+        <Toolbar>
+          {/*Inside the IconButton, we 
+           can render various icons*/}
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            {/*This is a simple Menu 
+             Icon wrapped in Icon */}
+            <MenuIcon />
+          </IconButton>
+          {/* The Typography component applies 
+           default font weights and sizes */}
+  
+          <Typography variant="h6" 
+            component="div" sx={{ flexGrow: 1 }}>
+            Title of Book
+          </Typography>
+          <Button color="inherit">Log Out</Button>
+        </Toolbar>
+      </AppBar>
+  );
+}
+
+export default Header;
